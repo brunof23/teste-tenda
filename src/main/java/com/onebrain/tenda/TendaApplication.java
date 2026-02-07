5cefed9030d8786676a0618cb0c19d435e047ea5
bruno.silva.32 <bruno.silva.32@ebserh.gov.br>
2026-02-07 16:36:57 -0300

    feat: estrutura inicial pelo spring io

---
+++ src/main/java/com/onebrain/tenda/TendaApplication.java
@@ -0,0 +1,13 @@
+package com.onebrain.tenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TendaApplication.class, args);
+	}
+
+}
